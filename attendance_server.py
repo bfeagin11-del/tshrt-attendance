f1279b8d9aea74b160c179ef4434f96dbf948873
bfeagin11-del <bfeagin11@gmail.com>
2026-03-26 19:31:18 -0500

    Update attendance_server.py

--- attendance_server.py
+++ attendance_server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from datetime import datetime, timedelta
 import os
 import json
-
+print("🔥🔥🔥 NEW VERSION LOADED 🔥🔥🔥")
 app = Flask(__name__)
 
 DATA_FILE = "roster_data.json"
